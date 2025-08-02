6e0fc9a02443d90c6b00a5e737fcbe9c5e95a5be
gwilhermealm <guilhermealmeidze@gmail.com>
2025-08-02 00:30:26 -0300

    novos exercicios de js

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('o console funcionou')
+var vel=60
+console.log(`a velocidade do seu carro é ${vel}`)
+if ( vel > 60){
+    console.log(`voce ultrapassou o limite de velocidade, MULTADO!`)
+}
+console.log('dirija sempre com cuidado!')
