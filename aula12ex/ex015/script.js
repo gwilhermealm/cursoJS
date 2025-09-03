d45d7476d08aabaf723bc8d2f5d81293a036946f
gwilhermealm <guilhermealmeidze@gmail.com>
2025-09-03 17:52:09 -0300

    exercicio 2

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,47 @@
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if(fano.value.length == 0 || Number(fano.value) >  ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id','foto')
+
+       if(fsex[0].checked){
+        genero = 'Homen'
+        if(idade >=0 && idade < 10){
+            //criança
+            img.setAttribute('src','img/crian-masc.png')
+
+        }else if (idade < 21){
+            //jovem
+        } else if (idade < 50){
+            //adulto
+        }else{
+           //idoso 
+        }
+       }else if (fsex[1].checked){
+        genero = 'mulher'
+         if(idade >=0  && idade <10 ){
+            //criança
+         }else if (idade < 21){
+            //jovem
+         } else if (idade < 50){
+            //adulto
+         }else{
+           //idoso 
+        }
+       }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+
+    }
+   
+      
+}
